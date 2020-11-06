be44f9f7654c9d1b8751312a3c526c88daca4a46
Lennert <triolenni@gmx.de>
2020-11-06 11:36:50 +0100

    2.2

---
+++ graphs/stem_properties.m
@@ -0,0 +1,24 @@
+function ax = stem_properties(tit, x_label, y_label,x)
+%sets properties for plot-graphs
+
+%tit = title of the plot
+%x_label = label for x-axis
+%y_label = label for y-axis
+%x = plotted graph
+
+if strcmpi(tit, '') == 1
+    title('unnamed graph');
+else title(tit);
+end
+
+ax = gca;
+
+xlim([0 length(x)+(length(x)/10)])
+ylim([min(x)-max(x)/10 max(x)+max(x)/10])
+
+xlabel(x_label);
+ylabel(y_label);
+grid on;
+grid minor;
+
+end
--- tools/faltung.m
+++ tools/faltung.m
@@ -1,9 +1,9 @@
-function w = faltung (x,u)
-kx= length (x) -1;
-ku= length (u) -1; 
-kw = kx+ku;
-w = zeros(1,kw+1);
-for k = 0:kw
-    for i ...
-    end;
+function y = faltung (x,u) 
+    len_y = (length(x)-1) + (length(u)-1);
+    y = zeros(1,len_y+1);
+    for k = 0:len_y
+        for i = 
+            
+        end
+    end
 end
