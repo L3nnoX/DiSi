f6237dac94ee8538f64c78cc6cd690cee0fa9110
Lennert <triolenni@gmx.de>
2020-11-20 13:15:27 +0100

    faltung und diff fertig

--- tools/difference.m
+++ tools/difference.m
@@ -1,7 +1,12 @@
-function [outputArg1,outputArg2] = difference(inputArg1,inputArg2)
-%DIFFERENCE Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+function y = difference(x)
+%  y[k] = x[k]-x[k-1]
 
+y = zeros(1,length(x));
+
+for k  = 1:length(x)
+    if k == 1
+        y(k) = x(k); %Annahme x(0)=0
+    else 
+        y(k) = x(k)-x(k-1);
+    end
+end
--- tools/faltung.m
+++ tools/faltung.m
@@ -1,9 +1,24 @@
 function y = faltung (x,u) 
-    len_y = (length(x)-1) + (length(u)-1);
-    y = zeros(1,len_y+1);
-    for k = 0:len_y
-        for i = 
-            
+%faltet x mit u  
+
+%beide Vektoren auf die selbe Länge bringen
+f_x = [x, zeros(1,length(u))];
+f_u = [u, zeros(1,length(x))];
+
+%Länge des gefalteten Signals bestimmen
+len_y = length(x) + length(u)-1;
+y = zeros(1,len_y);
+
+% ein Durchlauf pro Ausgabeelement in y
+    for i = 1:len_y
+% Suche nach der zu faltenden Stelle durch gegeneinander laufen beider Vektoren       
+        for j = 1:length(f_x)
+            if((i-j+1)>0)
+                y(i) = y(i) + f_x(j) * f_u(i-j+1);
+            end
         end
     end
+
+%Probe
+%falt = conv(x,u)
 end
