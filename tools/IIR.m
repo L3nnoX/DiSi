2ee8d7ef5f2ecf7ea0564d6ea0c392b36585b5e6
Lennert <triolenni@gmx.de>
2021-01-19 13:03:28 +0100

    protokoll weiter
    main: fir aus iir eingefügt

--- tools/IIR.m
+++ tools/IIR.m
@@ -1,17 +1,14 @@
 function y = IIR(x,ord,coef1, coef2)
-%IIR 
+%IIR-Filter
 % coef1 = [b0 b1 b2 b3 b4] bei n=4
-% coef2 = [c1 c2 c3 c4] bei n=4
+% coef2 = [c0 c1 c2 c3 c4] bei n=4
 
 n = ord;
 b = coef1;
 c = coef2;
-c = c.* (1/c(1));
 
 len = (length(x));
-% x_long = zeros(1, len);
-% x_long(n+1:length(x)+n) = x;
-u = zeros(1,len);
+u = zeros(1,len);    %Initialisierung der beiden Ausgänge
 y = zeros(1,len);
 reg1 = zeros(1,n+1); %alle Delayelemente auf 0
 reg2 = zeros(1,n+1); %alle Delayelemente auf 0
@@ -32,15 +29,13 @@ end
 %Rückgekoppelter Zweig
 for m = 1:len
         if m ==1
-            y(m) = u(m);
+            y(m) = u(m);          %Sonderfall des ersten Elements
         else
             reg2(1) = y(m-1);     %1. Registerstelle füllen mit rückgekoppeltem Ausgang
             y(m) = u(m) - sum(reg2(1:length(c)-1) .* c(2:end));   %"Rückkoplung"
                     
         end
     
-    reg2 = circshift(reg2, 1);    %Register um eine Stelle verschieben
-    
+    reg2 = circshift(reg2, 1);    %Register um eine Stelle verschieben  
 end
-
 end
